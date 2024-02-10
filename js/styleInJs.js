4f94438630bab95b87007ade993386f48476536a
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-10 11:47:28 +0600

    nodeChild, element child, node sibling, create element, append child, parent node

--- js/styleInJs.js
+++ js/styleInJs.js
@@ -18,3 +18,42 @@ let final = document.getElementById('result');
 
 final.classList.add('text-center');
 final.classList.add('large-text');
+
+
+// ---------- Node -----------
+console.log('NodeList, Htmlcollection, ParentNode, Childnodes, CreateElement');
+
+// node child --> #text around each element
+console.log(final.firstChild);
+console.log(final.childNodes);
+
+console.log(final.childNodes[1].nextSibling);
+console.log(final.childNodes[1].previousSibling);
+
+// element child
+console.log(final.childElementCount);
+
+console.log(final.lastElementChild.previousElementSibling);
+
+
+// --------- create element ----------
+const li = document.createElement('li');
+li.innerText = 'Brand New place to go'
+
+console.log(li);
+
+
+// ------ append child -------
+final.childNodes[3].appendChild(li);
+console.log(final);
+
+
+/**
+ * --------- parent node ----------
+ * 
+ * --> parentElement is same
+ * --> great parent is document. then null
+*/
+console.log(final.parentNode);
+console.log(final.parentElement.parentElement.parentNode.parentNode);
+console.log(final.parentElement.parentElement.parentNode.parentNode.parentNode);
